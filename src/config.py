aa7067de8c21cf8b071c552071a49188bee4c632
yodeng <yodeng@tju.edu.cn>
2022-03-29 16:44:27 +0800

    update config

--- src/config.py
+++ src/config.py
@@ -4,17 +4,27 @@ import configparser
 
 class Conf(configparser.ConfigParser):
     def __init__(self, defaults=None):
-        configparser.ConfigParser.__init__(self, defaults=None)
+        super(Conf, self).__init__(defaults=defaults)
 
     def optionxform(self, optionstr):
         return optionstr
+
+
+class Dict(dict):
+    def __getattr__(self, name):
+        return self[name]
+
+    def __setattr__(self, name, value):
+        self[name] = value
 
 
 class Config(object):
 
     def __init__(self, config_file=None):
         self.cf = []
-        self.info = {}
+        self.info = Dict()
+        if config_file is None:
+            return
         self._path = os.path.join(os.getcwd(), config_file)
         self.cf.append(self._path)
         if not os.path.isfile(self._path):
@@ -22,10 +32,10 @@ class Config(object):
         self._config = Conf()
         self._config.read(self._path)
         for s in self._config.sections():
-            self.info[s] = dict(self._config[s].items())
+            self.info[s] = Dict(dict(self._config[s].items()))
 
     def get(self, section, name):
-        return self.info.get(section, {}).get(name, None)
+        return self.info.get(section, Dict()).get(name, None)
 
     def update_config(self, config):
         self.cf.append(config)
@@ -34,25 +44,32 @@ class Config(object):
         c = Conf()
         c.read(config)
         for s in c.sections():
-            self.info.setdefault(s, {}).update(dict(c[s].items()))
+            self.info.setdefault(s, Dict()).update(dict(c[s].items()))
 
     def update_dict(self, **kwargs):
-        self.info.setdefault("args", {}).update(kwargs)
+        self.info.setdefault("args", Dict()).update(kwargs)
+
+    def write_config(self, configile):
+        with open(configile, "w") as fo:
+            for s, info in self.info.items():
+                fo.write("[%s]\n" % s)
+                for k, v in info.items():
+                    fo.write("%s = %s\n" % (k, v))
+                fo.write("\n")
 
     def __str__(self):
-        print(self.info)
+        return self.info
+
+    def __call__(self):
+        return self.info
 
 
 def load_config():
     configfile_home = os.path.join(os.path.expanduser("~"), ".runjobconfig")
-
     configfile_default = os.path.join(os.path.dirname(
         os.path.abspath(__file__)), 'runjobconfig')
-
     conf = Config(configfile_default)
-
     conf.update_config(configfile_home)
-
     return conf
 
 
