6ba9d767e367c5b22cf819d096bf221146032b3b
yodeng <yodeng@tju.edu.cn>
2023-06-21 10:53:27 +0800

    update setup

--- src/dag.py
+++ src/dag.py
@@ -1,27 +1,18 @@
 #!/usr/bin/env python
 # coding:utf-8
 
-
-from copy import copy, deepcopy
-from collections import deque
-
-from collections import OrderedDict
-
-
-class DAGValidationError(Exception):
-    pass
+from copy import copy
+from collections import deque, OrderedDict
 
 
 class DAG(object):
     """ Directed acyclic graph implementation. """
 
     def __init__(self):
-        """ Construct a new DAG with no nodes or edges. """
         self.reset_graph()
         self.all_nodes = set()
 
     def add_node(self, node_name, graph=None):
-        """ Add a node if it does not exist yet, or error out. """
         if not graph:
             graph = self.graph
         if node_name in graph:
@@ -36,14 +27,12 @@ class DAG(object):
             pass
 
     def delete_node(self, node_name, graph=None):
-        """ Deletes this node and all edges referencing it. """
         if not graph:
             graph = self.graph
         if node_name not in graph:
             raise KeyError('node %s does not exist' % node_name)
         graph.pop(node_name)
         self.all_nodes.remove(node_name)
-
         for node, edges in graph.items():
             if node_name in edges:
                 edges.remove(node_name)
@@ -55,7 +44,6 @@ class DAG(object):
             pass
 
     def add_edge(self, ind_node, dep_node, graph=None):
-        """ Add an edge (dependency) between the specified nodes. """
         if not graph:
             graph = self.graph
         if ind_node not in graph or dep_node not in graph:
@@ -63,7 +51,6 @@ class DAG(object):
         graph[ind_node].add(dep_node)
 
     def delete_edge(self, ind_node, dep_node, graph=None):
-        """ Delete an edge from he graph. """
         if not graph:
             graph = self.graph
         if dep_node not in graph.get(ind_node, []):
@@ -71,28 +58,23 @@ class DAG(object):
         graph[ind_node].remove(dep_node)
 
     def rename_edges(self, old_task_name, new_task_name, graph=None):
-        """ Change references to a task in existing edges. """
         if not graph:
             graph = self.graph
         for node, edges in graph.items():
-
             if node == old_task_name:
                 graph[new_task_name] = copy(edges)
                 del graph[old_task_name]
-
             else:
                 if old_task_name in edges:
                     edges.remove(old_task_name)
                     edges.add(new_task_name)
 
     def predecessors(self, node, graph=None):
-        """ Returns a list of all predecessors of the given node """
         if graph is None:
             graph = self.graph
         return [key for key in graph if node in graph[key]]
 
     def downstream(self, node, graph=None):
-        """ Returns a list of all nodes this node has edges towards. """
         if graph is None:
             graph = self.graph
         if node not in graph:
@@ -100,9 +82,6 @@ class DAG(object):
         return list(graph[node])
 
     def all_downstreams(self, node, graph=None):
-        """Returns a list of all nodes ultimately downstream
-        of the given node in the dependency graph, in
-        topological order."""
         if graph is None:
             graph = self.graph
         nodes = [node]
@@ -123,16 +102,11 @@ class DAG(object):
         )
 
     def all_leaves(self, graph=None):
-        """ Return a list of all leaves (nodes with no downstreams) """
         if graph is None:
             graph = self.graph
         return [key for key in graph if not graph[key]]
 
     def from_dict(self, graph_dict):
-        """ Reset the graph and build it from he passed dictionary.
-        The dictionary takes the form of {node_name: [directed edges]}
-        """
-
         self.reset_graph()
         for new_node in graph_dict:
             self.add_node(new_node)
@@ -144,21 +118,17 @@ class DAG(object):
                 self.add_edge(ind_node, dep_node)
 
     def reset_graph(self):
-        """ Restore the graph to an empty state. """
         self.graph = OrderedDict()
 
     def ind_nodes(self, graph=None):
-        """ Returns a set of all nodes in the graph with no dependencies. """
         if graph is None:
             graph = self.graph
-
         dependent_nodes = set(
             node for dependents in graph.values() for node in dependents
         )
         return [node for node in graph.keys() if node not in dependent_nodes]
 
     def validate(self, graph=None):
-        """ Returns (Boolean, message) of whether DAG is valid. """
         graph = graph if graph is not None else self.graph
         if len(self.ind_nodes(graph)) == 0:
             return (False, 'no independent nodes detected')
@@ -169,9 +139,6 @@ class DAG(object):
         return (True, 'valid')
 
     def topological_sort(self, graph=None):
-        """ Returns a topological ordering of the DAG.
-        Raises an error if this is not possible (graph is not valid).
-        """
         if graph is None:
             graph = self.graph
 
